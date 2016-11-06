cf134faa5c07343252c5b2a86c1ea1b714d795a2
tentakles <ollebergling@gmail.com>
2016-11-06 10:36:28 +0100

    ogg sound effects convertion

--- code/SoundHandler.py
+++ code/SoundHandler.py
@@ -3,12 +3,12 @@ import pygame
 class SoundHandler:
 	def __init__(self):
 
-		self.sound_shoot =  pygame.mixer.Sound('data/sound/270344__littlerobotsoundfactory__shoot-00.wav')
-		self.sound_dog_hit =  pygame.mixer.Sound('data/sound/270308__littlerobotsoundfactory__explosion-00.wav')
-		self.sound_missile_hit =  pygame.mixer.Sound('data/sound/270311__littlerobotsoundfactory__explosion-03.wav')
+		self.sound_shoot =  pygame.mixer.Sound('data/sound/270344__littlerobotsoundfactory__shoot-00.ogg')
+		self.sound_dog_hit =  pygame.mixer.Sound('data/sound/270308__littlerobotsoundfactory__explosion-00.ogg')
+		self.sound_missile_hit =  pygame.mixer.Sound('data/sound/270311__littlerobotsoundfactory__explosion-03.ogg')
 		self.mute = False
-		self.sound_win =  pygame.mixer.Sound('data/sound/270330__littlerobotsoundfactory__jingle-achievement-01.wav')
-		self.sound_fail =  pygame.mixer.Sound('data/sound/270329__littlerobotsoundfactory__jingle-lose-00.wav')
+		self.sound_win =  pygame.mixer.Sound('data/sound/270330__littlerobotsoundfactory__jingle-achievement-01.ogg')
+		self.sound_fail =  pygame.mixer.Sound('data/sound/270329__littlerobotsoundfactory__jingle-lose-00.ogg')
 
 	def play(self,sound,stop):
 		if stop is True:
